cdb8b90ec492e63046c7bec89c53fdbdc5051837
B Hos <brandonscotthosley@gmail.com>
2024-10-20 18:29:39 +0000

    Update Setup for SISL environment dependencies

--- setup.sh
+++ setup.sh
@@ -9,9 +9,7 @@ apt-get install tmux
 # install python requirements
 apt install -y python3-pip
 python3 -m pip install --upgrade pip
-# Due to dependency issues .txt method will not work
 # Python libs must be installed in groups
-
 pip install numpy==1.24.3
 pip install torch
 pip install swig
@@ -19,6 +17,10 @@ pip install box2d-py
 pip install gymnasium[box2d]
 pip install pettingzoo
 pip install ray[rllib,tune,air]
+
+# For SISL
+pip install pygame
+pip install pymunk
 
 # install WandB if it will be used
 echo -e "\n\n"
